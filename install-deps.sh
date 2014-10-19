c6b28847dd36961ba3812eb43502eba77c56c1e0
Matt Bilson <mattbilson@MattBook-Pro.local>
2014-10-19 11:35:48 +0100

    Tweaks

--- install-deps.sh
+++ install-deps.sh
@@ -4,43 +4,50 @@ chsh -s /bin/zsh
 echo "**\t Installing oh-my-zsh"
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
-
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
 echo "**\t Installing git-friendly"
 rm -rf ~/Development/tools/git-friendly
 git clone git://github.com/jamiew/git-friendly.git ~/Development/tools/git-friendly
-export PATH=~/Development/tools/git-friendly:$PATH
 
 # https://rvm.io
 # rvm for the rubiess
+echo "**\t Installing rvm"
 curl -L https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
-#add to .zshrc
-#echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.' >> ~/.zshrc
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-source ~/.rvm/scripts/rvm
-
-
 
 # homebrew!
+echo "**\t Installing Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/go/install)"
-export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
-
 
 # https://github.com/rupa/z
 # z, oh how i love you
+echo "**\t Installing z"
 git clone https://github.com/rupa/z.git ~/Development/tools/z
 chmod +x ~/Development/tools/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
-
-
 # https://github.com/thebitguru/play-button-itunes-patch
 # disable itunes opening on media keys
 cd ~/Development/tools
-git clone https://github.com/thebitguru/play-button-itunes-patch
+git clone https://github.com/thebitguru/play-button-itunes-patch play-button-itunes-patch
+open ./play-button-itunes-patch
 
 # install NVM
+echo "**\t Installing nvm"
 curl https://raw.githubusercontent.com/creationix/nvm/v0.17.2/install.sh | bash
 nvm install 0.10.32
-#nvm use stable
+
+
+echo "**\t ALL DONE! Opening some browser windows for apps..."
+
+open "http://google.com/chrome"
+open "http://spotify.com"
+open "http://skype.com"
+open "http://http://www.sublimetext.com/"
+open "http://www.videolan.org/vlc/index.en_GB.html"
+open "http://iterm2.com/"
+open "https://www.virtualbox.org/"
+open "https://www.dropbox.com/"
+open "https://justgetflux.com/"
+
+open .manual
